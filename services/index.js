8e97dee0e00dfc1490ddc00a9281576deaa43be3
Draxstep <113955176+Draxstep@users.noreply.github.com>
2026-02-17 15:46:53 -0500

    Api funcionando junto con DB

--- services/index.js
+++ services/index.js
@@ -1,1 +1,38 @@
-console.log("Funcionando");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors"; 
+import sequelize from "../config/database.js";
+
+dotenv.config();
+const app = express();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000" 
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['set-cookie']
+}));
+
+app.use(express.json());
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Conectado a la base de datos"))
+  .catch((err) => console.error("Error DB:", err));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
