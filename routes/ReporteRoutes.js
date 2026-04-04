a93ab4c8545c7142568a51790bf3e4a1badc84ae
Mabel Jimenez <jimenez.mabel230@gmail.com>
2026-04-04 12:10:58 -0500

    Add events popularity ranking endpoint
    Import getEventsByPopularity from ReporteController and register a new GET /rank route in ReporteRoutes to expose event popularity rankings. This adds the endpoint wiring so clients can retrieve events ordered by popularity.

--- routes/ReporteRoutes.js
+++ routes/ReporteRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 import {
     obtenerMetricasGenerales,
-    obtenerReporteVentasPorEvento
+    obtenerReporteVentasPorEvento,
+    getEventsByPopularity
 } from "../controllers/ReporteController.js";
 import { validarEventoIdReporte } from "../middlewares/ReporteValidator.js";
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.get("/metricas-generales", obtenerMetricasGenerales);
 router.get("/ventas/evento/:evento_id", validarEventoIdReporte, obtenerReporteVentasPorEvento);
+router.get("/rank", getEventsByPopularity);
 
 export default router;
