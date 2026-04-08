4a9608c750a0cc1a40e4526851cfe30d6d418abf
Mabel Jimenez <jimenez.mabel230@gmail.com>
2026-04-08 17:49:54 -0500

    Update errorHandler.js

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,4 @@
-export const errorHandler = (err, req, res, next) => {
+export const errorHandler = (err, req, res, _next) => {
     console.error(err.stack); 
     const statusCode = err.statusCode || 500;
     const message = err.statusCode ? err.message : "Error interno del servidor";
